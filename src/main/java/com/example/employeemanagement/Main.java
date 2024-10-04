0b1481246e2008d5b0f5b2bc8aa258db51cb1236
Sanjeev Panday <sanjeevpanday@Sanjeevs-MacBook-Pro.local>
2024-10-04 19:36:35 +0100

    Refactored Main class

--- src/main/java/com/example/employeemanagement/Main.java
+++ src/main/java/com/example/employeemanagement/Main.java
@@ -9,18 +9,28 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         List<Employee> employees = DataGenerator.generateEmployees();
-        List<String> empOlderThan40 = employees.stream().filter(e -> e.getAge() > 40).map(Employee::getName).toList();
-        System.out.println(empOlderThan40);
+        // Task 1 : Find all employees older than 40 and collect their names into a list.
+        List<String> empOlderThan40 = employees.stream()
+                .filter(e -> e.getAge() > 40)
+                .map(Employee::getName)
+                .toList();
 
-        List<String> uniqueDepartments = employees.stream().map(employee -> employee.getDepartment().name()).distinct().toList();
-        System.out.println(uniqueDepartments);
+        // Task 2: Create a list of all unique department names where employees work.
+        List<String> uniqueDepartments = employees.stream()
+                .map(employee -> employee.getDepartment().name())
+                .distinct()
+                .toList();
 
-        List<String> projects =  employees.stream().flatMap(employee -> employee.getProjects().stream()).map(Project::getProjectName).distinct().toList();
-        Stream<Project> streamOfProjects = employees.stream().flatMap(employee -> employee.getProjects().stream());
-        System.out.println(projects);
+        // Task 3: List all unique project names that employees are working on.
+        List<String> projects =  employees.stream()
+                .flatMap(employee -> employee.getProjects().stream())
+                .map(Project::getProjectName)
+                .distinct()
+                .toList();
 
-        Map<String,List<Employee>> groupedByDepartmentName = employees.stream().collect(Collectors.groupingBy(employee -> employee.getDepartment().name()));
+        // Task 4: Group employees by their departments.
+        Map<String,List<Employee>> groupedByDepartmentName = employees.stream()
+                .collect(Collectors.groupingBy(employee -> employee.getDepartment().name()));
 
-        System.out.println(groupedByDepartmentName);
     }
 }
